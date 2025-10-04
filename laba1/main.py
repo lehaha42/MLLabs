3cb3ae957bb5f9dfbf8e7de24bc0c834e5c0f9cd
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2025-10-04 10:01:31 +1000

    display fix

--- laba1/main.py
+++ laba1/main.py
@@ -5,11 +5,14 @@ import seaborn as sns
 
 class Main:
     def __init__(self, filepath: str):
+        pd.options.display.max_columns = 999
+        pd.options.display.width = 999  # для отображения всех колонок
         self.df = pd.read_csv(filepath)
 
     def main(self):
         print(self.df)  # вывести начало и конец файла
         self.df.info()  # вывод инфо о датасете
+        print(self.df.describe())  # анализ столбцов
 
 
 if __name__ == '__main__':
